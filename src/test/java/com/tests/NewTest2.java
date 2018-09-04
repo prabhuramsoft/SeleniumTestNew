6e05490adb3e6c6e5ec9840b82a508407f247684
Training_C2a.05.07 <Training_C2a.05.07@accenture.com>
2018-09-04 12:01:06 +0530

    good

---
+++ src/test/java/com/tests/NewTest2.java
@@ -0,0 +1,46 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+	WebDriver driver;
+	@BeforeMethod
+	public void beforelaunch()
+	{
+	System.out.println("before test");	
+	}
+	@Test
+	public void launchfirefox()
+	{
+		System.setProperty("webdriver.gecko.driver", "C:\\Softwares\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+	    driver=new FirefoxDriver();
+	    driver=new ChromeDriver();
+		driver.get("http://newtours.demoaut.com/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.name("userName")).sendKeys("tutorial");
+		driver.findElement(By.name("password")).sendKeys("tutorial");
+		driver.findElement(By.name("login")).click();
+		
+				
+	}
+	@AfterMethod
+	public void aftermethod()
+	{
+		System.out.println("after execution");
+	}
+
+}
